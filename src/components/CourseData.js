acbae400b29cbaf494ada410016ab0a45d8ab991
Noblelar <59696875+Noblelar@users.noreply.github.com>
2023-05-26 18:47:14 +0000

    cwa

--- src/components/CourseData.js
+++ src/components/CourseData.js
@@ -24,18 +24,20 @@ class CourseData extends Component {
                         event.target.value >= 70 ? "B" :
                             event.target.value >= 60 ? "C" :
                                 event.target.value >= 50 ? "D" :
-                                    "F"
+                                    event.target.value < 0 ? "Invalid Score" :
+                                        "F"
         })
         this.setState({
             weigthed_score:
-            event.target.value * this.props.c_hours
+            event.target.value > 100 || event.target.value < 0 ? "Score Range(0-100)":
+                event.target.value * this.props.c_hours
         })
 
 
     }
 
     render() {
-        
+
         return (
 
             < tr className="stripe-dark" >
@@ -47,7 +49,7 @@ class CourseData extends Component {
                     {
                         this.state.weigthed_score
                     }
-                    <input type="hidden" className={"sem"+this.props.period} value={this.state.weigthed_score}/>
+                    <input type="hidden" className={"sem" + this.props.period} value={this.state.weigthed_score} />
                 </td>
                 <td className="pa3">
                     {
--- src/components/ScoreBox.js
+++ src/components/ScoreBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const ScoreBox = ({ score, wieght }) => {
     return (
         <td className="pa3">
-            <input className="input-reset ba b--black-20 pa2 mb2 db w-100" 
+            <input className="input-reset grow ba b--black-20 pa2 mb2 db w-100" 
             type="number" 
             max={100} 
             min={0}
